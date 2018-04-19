109fae3699f35bea027571555ce1d14d52504059
luodea <2575202388@qq.com>
2018-04-19 17:34:26 +0800

    配置日志记录器

--- src/main/java/com/controller/Home.java
+++ src/main/java/com/controller/Home.java
@@ -1,10 +1,14 @@
 package com.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Home {
+    
+    private static final Logger logger = LogManager.getLogger(Home.class);
 
     /**
      * @Description:打开首页
@@ -13,8 +17,8 @@ public class Home {
      * @date 2018年4月19日
      */
     @RequestMapping("/")
-    public String Home() {
-	System.out.println("访问主页面");
+    public String index() {
+	logger.error("访问主页面");
 	return "index";
 
     }
